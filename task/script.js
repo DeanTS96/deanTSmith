4d0ced560fcfa075d2bddca5ed22a5c8ad8a4fb8
Dean Smith <dean96@live.co.uk>
2022-03-15 15:55:12 +0000

    ad

--- task/script.js
+++ task/script.js
@@ -1,17 +1,10 @@
 alert("helloooooo");
 
 
-
-const alertFun2 = () => {
-    alert("button shorthand");
-}
-
 const but = document.getElementById("butOne");
 
 //but.addEventListener("click", alertFun);
 
-but.onclick = alertFun2();
-
-const alertFun = () => {
-    alert("button");
-}
+but.onclick = () => {
+        alert("button");
+    };
