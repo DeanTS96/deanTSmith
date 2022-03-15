3a26fa69becee48a0a57a9233b783fc9f5b7ed4c
Dean Smith <dean96@live.co.uk>
2022-03-15 16:38:23 +0000

    first php

--- task/script.js
+++ task/script.js
@@ -1,6 +1,33 @@
-alert("helloooooo");
+/*alert("helloooooo");
 
 const but = document.getElementById("butOne");
 but.onclick = () => {
         but.style.color = "blue";
-    };
+    };*/
+
+    $('#butOne').click(function() {
+
+		$.ajax({
+			url: "getWeather.php",
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				icao: $('#inputOne').val()
+			},
+			success: function(result) {
+
+				console.log(JSON.stringify(result));
+
+				if (result.status.name == "ok") {
+
+					$('#results').html(result['data'][0]['clouds']);
+				}
+			
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				// your error code
+			}
+		}); 
+	
+	});
+
