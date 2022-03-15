622985f3abb46232edb8f012037f0cbd77f4b8e3
Dean Smith <dean96@live.co.uk>
2022-03-15 15:30:55 +0000

    button

--- task/script.js
+++ task/script.js
@@ -2,6 +2,6 @@ alert("helloooooo");
 
 const but = document.getElementById("butOne");
 but.onClick = function() {
-    document.getElementById("testing").style.color = 'red';
+    alert("button")
 };
 
