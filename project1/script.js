eed764ae9764e11b4f7179762b19a4647ca0a749
Dean Smith <dean96@live.co.uk>
2022-03-22 21:18:20 +0000

    map with my location

--- project1/script.js
+++ project1/script.js
@@ -1,8 +1,29 @@
-    $('#butOne').click(function() {
+    /*$('#butOne').click(function() {
         navigator.geolocation.getCurrentPosition((position) => {
             console.log(position);
         });
+    });*/
+
+    let lat = 0;
+    let lng = 0;
+    console.log(lat);
+
+    let whereUR = navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+        lat = position.coords.latitude; 
+        lng = position.coords.longitude;
+        console.log(lat, lng);
+
+        let map = L.map('map').setView([lat, lng], 13);
+        L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
     });
 
-    let map = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
+    console.log(lat);
+
+    setTimeout(function(){
+        console.log(lat)
+    }, 1000);
+
+        
+
+  
