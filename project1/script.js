c85f6be4f64f2cb29e3e8c824224e9a122b70201
Dean Smith <dean96@live.co.uk>
2022-03-24 15:46:21 +0000

    finding lat & lng

--- project1/script.js
+++ project1/script.js
@@ -275,6 +275,47 @@
 	});
 
 
+	$('#butSeven').click(function() {
+        console.log("preessed button 7, get lat, lng");
+
+		$.ajax({
+			url: "TEMPgetLatLng.php",
+			type: 'POST',
+			dataType: 'json',
+			data: {
+                country: $("select option:selected").html(),
+				code: $("select").val()
+			},
+			success: function(result) {
+				console.log(JSON.stringify(result));
+
+				if (result.status.name == "ok") {
+
+					//$('#paragraph').html(result.data);
+					//$('#countries').append($("<option>").val("HI").text("NONO"));
+					//$('#paragraph').html(result['data'][0]['languages']);
+					/*result.data.forEach(
+						function(countryObject) {
+							$('#countries').append($("<option>").val(countryObject.iso).text(countryObject.country));
+							//let newOption = document.createElement("option");
+							//newOption.innerHTML = countryObject.country;
+							//document.getElementById("countries").appendChild(newOption);
+
+							//$('#countries').append($("<option>").val("GB").text("Britain"));
+							
+							//console.log(countryObject.country);
+							//console.log(countryObject);
+					});*/
+				}
+			
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				// your error code
+			}
+		}); 	
+	});
+
+
 	/*let newOption = document.createElement("option");
 	newOption.value = "GB";
 	newOption.innerHTML = "Britain";
